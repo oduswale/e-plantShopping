4a74257ae1ec4e0e0e1cdde0c1ad82f9993e1712
Odukogbe Adewale <oduswale1@gmail.com>
2025-09-13 15:44:54 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+
+// Import Provider to make Redux store available throughout the app
 import { Provider } from 'react-redux'
+
+// Import the global Redux store configured in store.js
 import store from './store.js'
+
+// Render the React application
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* Wrap App with Provider so all components can access Redux store */}
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
   </React.StrictMode>,
 )
